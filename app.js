8c75d555dbcd73a164abb75db9e6a3d3b4a849f8
sachingupta67 <sachingupta769@outlook.com>
2020-08-15 16:29:26 +0530

    dynamic port added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("welcome to home");
 });
-app.listen(3000, () => {
+const PORT = process.env.port || 3000;
+app.listen(PORT, () => {
   console.log("server is reunning");
 });
